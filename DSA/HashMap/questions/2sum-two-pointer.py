e42ce7e4d5019dbd0936e1155e2159a132419ae2
Rahul Ray <FossXpert@outlook.com>
2026-07-12 16:57:44 +0530

    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/

--- DSA/HashMap/questions/2sum-two-pointer.py
+++ DSA/HashMap/questions/2sum-two-pointer.py
@@ -1,7 +1,16 @@
 class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        num = numbers
+        j = len(num)-1
+        i=0
+        while(i<j):
+            x = num[i] + num[j]
+            if x == target:
+                return [i+1,j+1]
+            if x > target:
+                j-=1
+            if x < target:
+                i+=1
+        return [-1,-1] 
 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        return ans
-        
-# https://leetcode.com/problems/two-sum/
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
