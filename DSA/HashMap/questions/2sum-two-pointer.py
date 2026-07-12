11a07c81f1fcc310d5b0bfba57de1bef7444a47c
Rahul Ray <FossXpert@outlook.com>
2026-07-12 15:06:53 +0530

    NTSBUI-00000

--- DSA/HashMap/questions/2sum-two-pointer.py
+++ DSA/HashMap/questions/2sum-two-pointer.py
@@ -1,29 +1,7 @@
 class Solution:
-    # def twoSum(self, nums: List[int], target: int) -> List[int]:
-    #     # O(n2)
-    #     ans=[]
-    #     for i in range(0,len(nums)):
-    #         for j in range(i+1,len(nums)):
-
-    #             if nums[i] + nums[j] == target:
-    #                 ans.append(i)
-    #                 ans.append(j)
-    #                 return ans
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #O(n)
-        mapp = {}
-        for i in range(0, len(nums)):
-            mapp[nums[i]] = i
-
-        ans = []
-        for i in range(0, len(nums)):
-            x = target - nums[i]
-            if x in mapp and i != mapp[x]:
-                ans.append(i)
-                ans.append(mapp[x])
-                return ans
-
+        
         return ans
         
 # https://leetcode.com/problems/two-sum/
