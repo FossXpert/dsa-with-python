302d3cb637effcc6802da557c683e20d30496420
Rahul Ray <FossXpert@outlook.com>
2026-07-30 15:46:21 +0530

    k

---
+++ DSA/Bit Manipulation/XOR/Notes.md
@@ -0,0 +1,6 @@
+1. Core Properties
+* Self-Inverse: 
+Applying the XOR operation with the same value twice cancels it out. If you take a number $A$ and XOR it with $B$, and then XOR the result with $B$ again, you get $A$ back:$$(A \oplus B) \oplus B = A$$
+* Self-Inverse: $x \oplus x = 0$
+* Identity: $x \oplus 0 = x$
+* Commutative & Associative: Order does not matter.Reversibility: If $a \oplus b = c$, then $a \oplus c = b$ and $b \oplus c = a$.
---
+++ DSA/Bit Manipulation/XOR/questions/1310. XOR Queries of a Subarray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        # ^ - xor symbol
+        # optimising by using xor property : 
+        # Self-Inverse: Applying the XOR operation with the same value twice cancels it out. If you take a number A and XOR it with B, and then XOR the result with B again, you get A back i.e (A ^ B) ^ B = A or (A ^ B) ^ A = B
+
+        xorArr,ans=[],[]
+        n = len(arr)
+        xorArr.append(arr[0])
+
+        for i in range(1, n):
+            xorArr.append(arr[i] ^ xorArr[i - 1])
+
+        for left, right in queries:
+            if left == 0:
+                ans.append(xorArr[right])
+            else:
+                ans.append(xorArr[right] ^ xorArr[left - 1])
+
+        return ans
